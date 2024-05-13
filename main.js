cddd8e8efb43153e0f1a52cece88db8a492eb5ed
suharyadi <suharyadi21@outlook.com>
2024-05-13 19:37:31 +0700

    - fixing

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const amqplib = require('amqplib/callback_api');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // Konfigurasi koneksi RabbitMQ
-const queue = 'sendWaPengaduan'; //chanel
-const rabbitmqHost = 'localhost';
-const rabbitmqPort = 5672;
-const rabbitmqUser = 'guest';
-const rabbitmqPassword = 'guest';
+const queue = process.env.RABBITQUEUESENDMESSAGE //chanel
+const rabbitmqHost = process.env.RABBITMQHOST;
+const rabbitmqPort = process.env.RABBITMQPORT;
+const rabbitmqUser = process.env.RABBITMQUSER;
+const rabbitmqPassword = process.env.RABBITMQPASSWORD;
 
 const client = new Client({
     webVersionCache: {
         type: 'remote',
         authStrategy: new LocalAuth(),
-        puppeteer: {
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        },
+        // puppeteer: {
+        //     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        // },
         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2410.1.html',
     }
 });
@@ -38,7 +41,6 @@ client.initialize();
 function sendMessageWhatsApp(number, text) {
     const chatId = number.substring(1) + '@c.us';
     client.sendMessage(chatId, text);
-    console.log(`Pesan "${text}" berhasil dikirim ke nomor tujuan ${number}`);
 }
 
 // Membuat koneksi ke RabbitMQ
@@ -55,14 +57,13 @@ function createRabbitMQConnection() {
             ch2.consume(queue, (msg) => {
                 if (msg !== null) {
                     const content = msg.content.toString();
-                    console.log(`Pesan dari RabbitMQ: ${content}`);
-
                     try {
                         const data = JSON.parse(content);
                         const message = data.message;
                         const targetNumber = data.recipient;
                         
                         sendMessageWhatsApp(targetNumber, message);
+                        console.log(`Pesan dikirim ke : ${content}`);
                     } catch (error) {
                         console.error('Error parsing JSON message:', error);
                     }
